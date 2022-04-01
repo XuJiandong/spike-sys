861f55fdc4819a83d8a8f9d50a1cd6b48d34cdd4
xjd <xjd@cryptape.com>
2022-04-01 16:03:24 +0800

    Fix string

--- examples/disassembler.rs
+++ examples/disassembler.rs
@@ -3,13 +3,12 @@ use std::{ffi::CStr, os::raw::c_char};
 use spike_sys::*;
 
 fn to_string(str: &[u8]) -> String {
-    let p = unsafe {
+    unsafe {
         let p = str.as_ptr();
         CStr::from_ptr(p as *const c_char)
             .to_string_lossy()
             .into_owned()
-    };
-    p.clone()
+    }
 }
 
 pub fn main() {
